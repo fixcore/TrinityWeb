4bd274de9e8a2e8676c0b9fdaef44c0717bba57e
Qblolz <frostQb@mail.ru>
2018-01-26 12:40:34 +0300

    armory

--- frontend/modules/armory/models/SearchForm.php
+++ frontend/modules/armory/models/SearchForm.php
@@ -13,12 +13,12 @@ use common\models\chars\Characters;
  */
 class SearchForm extends Model
 {
-    public $realm_id;
+    public $server;
     public $query = '';
     
     public function __construct($config = array()) {
         parent::__construct($config);
-        $this->realm_id = Yii::$app->CharactersDbHelper->getServerId();
+        $this->server = Yii::$app->CharactersDbHelper->getServerName();
     }
     
     /**
@@ -27,8 +27,8 @@ class SearchForm extends Model
     public function rules()
     {
         return [
-            [['realm_id', 'query'], 'required'],
-            ['realm_id', 'integer'],
+            [['server', 'query'], 'required'],
+            ['server', 'string'],
             ['query', 'string', 'min' => 2],
         ];
     }
@@ -36,13 +36,36 @@ class SearchForm extends Model
     public function attributeLabels()
     {
         return [
-            'realm_id' => Yii::t('common', 'Игровой мир'),
-            'query' => Yii::t('armory', 'Найти персонажа'),
+            'server' => Yii::t('common', 'Игровой мир'),
+            'query' => Yii::t('armory', 'Строка поиска'),
         ];
     }
     
-    public function findCharacters($params) {
-        
+    public function getServers() {
+        $servers = Yii::$app->CharactersDbHelper->getServers(true);
+        $data = [];
+        foreach($servers as $server) {
+            $data[$server] = $server;
+        }
+        return $data;
     }
+    
+    public function findCharacters($params) {
+        $this->load($params);
+        $dataProvider = new ActiveDataProvider([
+			'query' => Characters::find()->where(['like','name',$this->query])->orderBy(['guid' => SORT_DESC])->asArray(),
+			'pagination' => [
+				'pageSize' => 10,
+			],
+		]);
+        $data = Yii::$app->cache->get(Yii::$app->request->url);
+        if($data === false) {
+            $data = $dataProvider->getModels();
+            Yii::$app->cache->set(Yii::$app->request->url,$data,Yii::$app->params['cache_armory_search']);
+        }
+        return $data;
+    }
+    
+    public function formName() {return '';}
     
 }
--- frontend/modules/armory/views/main/index.php
+++ frontend/modules/armory/views/main/index.php
@@ -6,14 +6,17 @@ use yii\bootstrap\ActiveForm;
 
 ?>
 <?php $form = ActiveForm::begin([
-    'id' => 'armory-form',
+        'id' => 'armory-form',
+        'method' => 'get',
+        'action' => ['/armory']
     ]); ?>
     <div class="row">
         <div class="col-xs-push-2 col-xs-8 col-sm-push-2 col-sm-8 col-md-push-3 col-md-6 col-push-3 col-lg-6">
             <div class="row">
                 <div class="col-md-4">
-                    <?php echo $form->field($searchModel, 'realm_id')->dropDownList(Yii::$app->CharactersDbHelper->getServers(true),[
+                    <?php echo $form->field($searchModel, 'server')->dropDownList($searchModel->getServers(),[
                             'prompt' => Yii::t('cp','Выберите сервер'),
+                            'name' => 'server',
                         ])->label(false) ?>
                 </div>
                 <div class="col-md-7">
@@ -21,7 +24,7 @@ use yii\bootstrap\ActiveForm;
                 </div>
                 <div class="col-md-1">
                     <div class="form-group text-center-sm text-center-xs">
-                        <?php echo Html::submitButton(Yii::t('common', 'Поиск'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                        <?php echo Html::submitButton(Yii::t('common', 'Поиск'), ['class' => 'btn btn-primary']) ?>
                     </div>
                 </div>
             </div>
