486c40685b2affe9b33cf5c6453c9599654c04ba
Qblolz <frostQb@mail.ru>
2018-01-29 15:17:16 +0300

    view page - part2

--- frontend/modules/armory/views/character/character.php
+++ frontend/modules/armory/views/character/character.php
@@ -140,6 +140,161 @@ use yii\helpers\Url;
                             ])?>
                         </div>
                     </div>
+                    <hr/>
+                    <div id="amory_character_switch">
+                        <ul id="armory_character_menu" class="nav nav-tabs">
+                            <li class="active">
+                                <a href="#melee" data-toggle="tab">
+                                    <?=Yii::t('armory','Ближний бой')?>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#range" data-toggle="tab">
+                                    <?=Yii::t('armory','Дальний бой')?>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#magic" data-toggle="tab">
+                                    <?=Yii::t('armory','Магия')?>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#defence" data-toggle="tab">
+                                    <?=Yii::t('armory','Защита')?>
+                                </a>
+                            </li>
+                            <li class="previous-tab">
+                                <a href="">
+                                    <i class="fa rf-white fa-arrow-left"></i>
+                                </a>
+                            </li>
+                            <li class="next-tab">
+                                <a href="">
+                                    <i class="fa rf-white fa-arrow-right"></i>
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="melee">
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Урон: {number}', [
+                                        'number' => $data['stats']['melee_damage_from'] . ' - ' . $data['stats']['melee_damage_to'],
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Скорость атаки: {number}', [
+                                        'number' => number_format($data['stats']['expertiseBaseAttackPct'], 0, '.', '.'),
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Сила атаки: {number}', [
+                                        'number' => number_format($data['stats']['attackPower'], 0, '.', '.'),
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Меткость: {number}', [
+                                        'number' => number_format($data['stats']['hitMelee'], 0, '.', '.'),
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Крит. удар: {number}%', [
+                                        'number' => number_format($data['stats']['critPct'], 2, '.', '.'),
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Мастерство: {number}', [
+                                        'number' => number_format($data['stats']['experience'], 0, '.', '.'),
+                                    ])?>
+                                </div>
+                            </div>
+                            <div class="tab-pane" id="range">
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Урон: {number}', [
+                                        'number' => $data['stats']['range_damage_from'] . ' - ' . $data['stats']['range_damage_to'],
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Скорость атаки: {number}', [
+                                        'number' => number_format($data['stats']['expertiseOffAttackPct'], 0, '.', '.'),
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Сила атаки: {number}', [
+                                        'number' => number_format($data['stats']['rangedAttackPower'], 0, '.', '.'),
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Меткость: {number}', [
+                                        'number' => number_format($data['stats']['hitRanged'], 0, '.', '.'),
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Крит. удар: {number}%', [
+                                        'number' => number_format($data['stats']['rangedCritPct'], 2, '.', '.'),
+                                    ])?>
+                                </div>
+                            </div>
+                            <div class="tab-pane" id="magic">
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Сила заклинания: {number}', [
+                                        'number' => number_format($data['stats']['spellPower'], 0, '.', '.'),
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Доп. лечение: {number}', [
+                                        'number' => number_format($data['stats']['spellHealing'], 0, '.', '.'),
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Меткость: {number}', [
+                                        'number' => number_format($data['stats']['hitSpell'], 0, '.', '.'),
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Крит. удар: {number}%', [
+                                        'number' => number_format($data['stats']['spellCrit'], 2, '.', '.'),
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Скорость: {number}', [
+                                        'number' => number_format($data['stats']['hasteSpell'], 0, '.', '.'),
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Вост. маны: {number}', [
+                                        'number' => $data['stats']['mp5'],
+                                    ])?>
+                                </div>
+                            </div>
+                            <div class="tab-pane" id="defence">
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Защита: {number}', [
+                                        'number' => number_format($data['stats']['defence'], 2, '.', '.'),
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Уклонение: {number}%', [
+                                        'number' => number_format($data['stats']['dodgePct'], 2, '.', '.'),
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Парирование: {number}%', [
+                                        'number' => number_format($data['stats']['parryPct'], 2, '.', '.'),
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Блок: {number}%', [
+                                        'number' => number_format($data['stats']['blockPct'], 2, '.', '.'),
+                                    ])?>
+                                </div>
+                                <div class="character_stat">
+                                    <?=Yii::t('armory','Устойчивость: {number}', [
+                                        'number' => number_format($data['stats']['resilience'], 0, '.', '.'),
+                                    ])?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="col-xs-3 col-sm-1 col-md-2 col-lg-2">
